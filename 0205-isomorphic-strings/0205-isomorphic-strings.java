d866217da80fc80d2c979f208c08356e8e108dfa
Dharshini.M.K <mkdharshini24@gmail.com>
2024-09-30 05:39:33 +0530

    Time: 5 ms (97.42%), Space: 42.5 MB (26.09%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,24 +1,19 @@
-class Solution {
-    public boolean isIsomorphic(String s, String t) {
+class Solution
+{
+    public boolean isIsomorphic(String s, String t)
+    {
         if (s.length() != t.length()) return false;
-        
-        // Two arrays to store mappings from s to t and t to s
         int[] sMap = new int[256];
-        int[] tMap = new int[256];
-        
-        for (int i = 0; i < s.length(); i++) {
+        int[] tMap = new int[256];       
+        for (int i = 0; i < s.length(); i++)
+        {
             char sChar = s.charAt(i);
-            char tChar = t.charAt(i);
-            
-            // If the current character from s maps to a different character in t, return false
-            if (sMap[sChar] != tMap[tChar]) {
-                return false;
-            }
-            
+            char tChar = t.charAt(i);           
+            if (sMap[sChar] != tMap[tChar])
+                return false;            
             sMap[sChar] = i + 1;
             tMap[tChar] = i + 1;
         }
-        
         return true;
     }
 }
