b5709d190bdec16ea724fc9574b1326917a1489b
Dharshini.M.K <dharshinimk.22cse@kongu.edu>
2025-02-16 13:22:55 +0530

    Time: 0 ms (100%), Space: 40.9 MB (67.88%) - LeetHub

--- 0492-construct-the-rectangle/0492-construct-the-rectangle.java
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.java
@@ -3,9 +3,7 @@ class Solution {
         for(int i=(int)Math.sqrt(area);i>=1;i--)
         {
             if(area%i==0)
-            {
                 return new int[]{Math.max(i,area/i),Math.min(i,area/i)};
-            }
         }
         return new int[]{area,1};
     }
