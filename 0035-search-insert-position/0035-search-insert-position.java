3913ae5845ba5da6f7aecf6e65fbe0cc49ab3b48
Dharshini.M.K <dharshinimk.22cse@kongu.edu>
2025-02-17 14:44:14 +0530

    Time: 0 ms (100%), Space: 43.2 MB (27.69%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,14 @@
+class Solution
+{
+    public int searchInsert(int[] nums, int target)
+    {
+        for(int i=nums.length-1;i>=0;i--)
+        {
+            if(nums[i]==target)
+                return i;
+            if(nums[i]<target)
+                return i+1;
+        }
+        return 0;
+    }
+}
