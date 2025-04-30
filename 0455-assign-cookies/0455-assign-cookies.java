24e8eb6fcc956c453dd3540fa32d62d32b3f1324
Dharshini.M.K <dharshinimk.22cse@kongu.edu>
2025-04-30 11:34:46 +0530

    Time: 10 ms (80.64%), Space: 48 MB (98.15%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,18 +1,18 @@
 import java.util.Arrays;
 
-class Solution {
-    public int findContentChildren(int[] g, int[] s) {
+class Solution
+{
+    public int findContentChildren(int[] g, int[] s)
+    {
         Arrays.sort(g); 
         Arrays.sort(s); 
-
-        int child = 0, cookie = 0;
-        while (child < g.length && cookie < s.length) {
-            if (s[cookie] >= g[child]) {
-                child++; // content child
-            }
-            cookie++; // move to next cookie
+        int child=0,cookie=0;
+        while (child<g.length&&cookie< s.length)
+        {
+            if(s[cookie]>=g[child])
+                child++; 
+            cookie++; 
         }
-
         return child;
     }
 }
